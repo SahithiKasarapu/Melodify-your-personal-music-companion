9b8ab86c63c493122f38b24b406fdfa845ad14c3
Tagore Charith <122634098+charithtagore@users.noreply.github.com>
2023-10-07 20:42:05 -0500

    Splash Screen activity is added

--- app/src/main/java/com/example/melodify_your_personal_music_companion/SplashScreenActivity.java
+++ app/src/main/java/com/example/melodify_your_personal_music_companion/SplashScreenActivity.java
@@ -1,8 +1,10 @@
 package com.example.melodify_your_personal_music_companion;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Button continueButton = findViewById(R.id.continueBT);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashScreenActivity.this, ViewSongActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
