ac5211cbabbf805f982e7f7d79197574c6ec97ce
SahithiKasarapu <sahithikasarapu@gmail.com>
2023-10-28 01:03:07 -0500

    commit

--- app/src/main/java/com/example/melodify_your_personal_music_companion/SplashScreenActivity.java
+++ app/src/main/java/com/example/melodify_your_personal_music_companion/SplashScreenActivity.java
@@ -11,17 +11,6 @@ import android.widget.ImageView;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-        ImageView continueButton = findViewById(R.id.melodify_Image);
-        continueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+
+
 }
